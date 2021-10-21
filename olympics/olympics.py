e47ba200fbba5baa70b03c24a9086cf04d417bed
Lev <shusterl@carleton.edu>
2021-10-20 21:14:59 -0700

    refactored code

--- olympics/olympics.py
+++ olympics/olympics.py
@@ -6,13 +6,15 @@
 
 import argparse, sys, psycopg2, config
 
-# set to false if you don't want my program to install the dependency that my third option relies on
+# set to false if you don't want my program to install the dependency that my third option 
+# relies on (you can test all the required content besides the "create your own operation" 
+# without installing any additional dependencies")
 SHOULD_INSTALL_WIKIPEDIA = True 
 
 
 ''' 
-    set up get_parsed_argument and psycopg2, direct parse results to 
-    proper sql method, and then print the results.
+    set up get_parsed_argument and psycopg2 then direct parse results to 
+    proper sql method.
 '''
 def main(argv):
     try:
@@ -29,14 +31,15 @@ def main(argv):
         print_athletes(connection, str(arguments.athletes))
     elif arguments.country_info:
         print_country_info(connection)
+    connection.close()
+
 
 def get_parsed_argument():
     parser = argparse.ArgumentParser(description='Process data on ~60 years of olympics.               original data from https://www.kaggle.com/heesoo37/120-years-of-olympic-history-athletes-and-results               Program by Lev Shuster')
     
-    # parse searches
     parser.add_argument('--athletes','-a',help='List the names of all the athletes from a specified NOC')
     parser.add_argument('--golds','-g',action='store_true',help='List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.')
-    parser.add_argument('--country_info','-i',action='store_true',help='Displays the wikipedia introductions to each country that has exacly two gold metals')
+    parser.add_argument('--country_info','-i',action='store_true',help='Displays the wikipedia introductions to each country that has exacly two gold metals. This is done so the user can learn about some of the less know countries sucsess at the olympics')
 
     return parser.parse_args()
 
@@ -55,6 +58,7 @@ def print_gold(connection):
     print('===== All NOCs and the number of their gold metals =====')
     for row in cursor:
         print(row[0], row[1])
+
 
 def print_athletes(connection, input_NOC):
     # SQL Call
@@ -76,14 +80,17 @@ def print_athletes(connection, input_NOC):
     for row in cursor:
         print(row[0])
 
+
 '''
     this function is helpful because it gets the user to learn about the smaller/less sucsessful countries that have still sucseeded at the olympics
 '''
 def print_country_info(connection):
-    NUMBER_OF_LINES_TO_SENTENCES = 3
+    NUMBER_OF_SENTENCES_TO_PRINT = 3
+    NUMBER_OF_GOLD_METALS_TO_FOCUS_ON = 2
+
     if SHOULD_INSTALL_WIKIPEDIA:
 
-        # Installs needed dependences and clears the output of pip
+        # Installs/Imports needed dependences and clears the output of pip
         import subprocess, os
         subprocess.check_call([sys.executable, "-m", "pip", "install", "wikipedia"])
         os.system('cls' if os.name=='nt' else 'clear')
@@ -98,21 +105,23 @@ def print_country_info(connection):
                         WHERE noc_regions.NOC_abbreviation = athlete_NOC_event_game_metal.NOC_abbreviation 
                         GROUP BY noc_regions.NOC_abbreviation) AS metal_count
                         WHERE noc_regions.NOC_abbreviation = metal_count.NOC_abbreviation
-                        AND metal_count.count = 2;'''
-            cursor.execute(query)
+                        AND metal_count.count = %s;'''
+            cursor.execute(query, (NUMBER_OF_GOLD_METALS_TO_FOCUS_ON,))
         except Exception as e:
             print(e)
             exit()
 
         # Display Results
         for row in cursor:
-            print("\n\n---Introduction to "+row[0]+" from wikipedia---")
+            country = row[0]
+            print("\n\n---Introduction to "+country+" from wikipedia---")
             try:
-                print(wikipedia.summary(row[0], sentences = NUMBER_OF_LINES_TO_SENTENCES))
+                print(wikipedia.summary(country, sentences = NUMBER_OF_SENTENCES_TO_PRINT))
             except wikipedia.exceptions.PageError:
                 print("No Data Availible")
     else:
-        print("to see my personal fucntion please allow this program to dowload the wikipedia package by setting SHOULD_INSTALL_WIKIPEDIA to True")
+        print("To see my personal function please allow this program to dowload the wikipedia package by setting SHOULD_INSTALL_WIKIPEDIA to True")
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
