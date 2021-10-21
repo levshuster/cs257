f95c792d82f9a3bf3677d2792e958a3ea74f923f
Lev <shusterl@carleton.edu>
2021-10-20 20:55:06 -0700

    finished olympics.py

--- olympics/olympics.py
+++ olympics/olympics.py
@@ -26,7 +26,7 @@ def main(argv):
     if arguments.golds:
         print_gold(connection)
     elif arguments.athletes:
-        print_athletes(connection, arguments.athletes)
+        print_athletes(connection, str(arguments.athletes))
     elif arguments.country_info:
         print_country_info(connection)
 
@@ -52,21 +52,35 @@ def print_gold(connection):
         exit()
 
     # Display Results
-    print("golds")
     print('===== All NOCs and the number of their gold metals =====')
     for row in cursor:
         print(row[0], row[1])
 
-def print_athletes(input_NOC):
+def print_athletes(connection, input_NOC):
     # SQL Call
+    try:
+        cursor = connection.cursor()
+        query =  '''SELECT athlete.whole_name FROM athlete, athlete_NOC_event_game_metal, noc_regions
+                    WHERE athlete.id = athlete_NOC_event_game_metal.athlete_ID
+                    AND   noc_regions.NOC_abbreviation=athlete_NOC_event_game_metal.NOC_abbreviation
+                    AND  (noc_regions.NOC_abbreviation=%s OR noc_regions.NOC_name=%s)
+                    GROUP BY athlete.whole_name
+                    ORDER BY athlete.whole_name;'''
+        cursor.execute(query, (input_NOC,input_NOC,))
+    except Exception as e:
+        print(e)
+        exit()
 
     # Display Results
-    print("athletes")
+    print('===== List athletes from ' + input_NOC+'=====')
+    for row in cursor:
+        print(row[0])
 
 '''
     this function is helpful because it gets the user to learn about the smaller/less sucsessful countries that have still sucseeded at the olympics
 '''
-def print_country_info():
+def print_country_info(connection):
+    NUMBER_OF_LINES_TO_SENTENCES = 3
     if SHOULD_INSTALL_WIKIPEDIA:
 
         # Installs needed dependences and clears the output of pip
@@ -76,10 +90,29 @@ def print_country_info():
         import wikipedia
         
         # SQL Call
+        try:
+            cursor = connection.cursor()
+            query =  '''SELECT noc_regions.NOC_name FROM noc_regions,
+                        (SELECT noc_regions.NOC_abbreviation, COUNT(CASE WHEN athlete_NOC_event_game_metal.metal = 'Gold' THEN 1 END) 
+                        FROM noc_regions, athlete_NOC_event_game_metal 
+                        WHERE noc_regions.NOC_abbreviation = athlete_NOC_event_game_metal.NOC_abbreviation 
+                        GROUP BY noc_regions.NOC_abbreviation) AS metal_count
+                        WHERE noc_regions.NOC_abbreviation = metal_count.NOC_abbreviation
+                        AND metal_count.count = 2;'''
+            cursor.execute(query)
+        except Exception as e:
+            print(e)
+            exit()
 
         # Display Results
-        print("country info")
-    
+        for row in cursor:
+            print("\n\n---Introduction to "+row[0]+" from wikipedia---")
+            try:
+                print(wikipedia.summary(row[0], sentences = NUMBER_OF_LINES_TO_SENTENCES))
+            except wikipedia.exceptions.PageError:
+                print("No Data Availible")
+    else:
+        print("to see my personal fucntion please allow this program to dowload the wikipedia package by setting SHOULD_INSTALL_WIKIPEDIA to True")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
