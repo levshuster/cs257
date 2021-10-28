da1cb48c3e261bf9b789c8d28f3bd10b440339c1
Lev <shusterl@carleton.edu>
2021-10-28 16:15:26 -0700

    finished assigment

--- olympics/olympics-api.py
+++ olympics/olympics-api.py
@@ -12,36 +12,39 @@ import sys, argparse, flask, json
 # to acsess sql database
 import psycopg2, config
 
-    
+ 
 app = flask.Flask(__name__)
 
-try:
-    connection = psycopg2.connect(database=config.database, user=config.user, password=config.password)
-except Exception as e:
-    print(e)
-    exit()
+def get_connection():
+    try:
+        connection = psycopg2.connect(database=config.database, user=config.user, password=config.password)
+    except Exception as e:
+        print(e)
+        exit()
+    return connection
 
 
 
-@app.route('/')
-def hello():
-    return 'Hello, Citizen of CS257.'
+# @app.route('/')
+# def hello():
+#     return 'Hello, Citizen of CS257.'
 
 
-'''    
-    REQUEST: /games
+# '''    
+#     REQUEST: /games
 
-    RESPONSE: a JSON list of dictionaries, each of which represents one
-    Olympic games, sorted by year. Each dictionary in this list will have
-    the following fields.
+#     RESPONSE: a JSON list of dictionaries, each of which represents one
+#     Olympic games, sorted by year. Each dictionary in this list will have
+#     the following fields.
 
-    id -- (INTEGER) a unique identifier for the games in question
-    year -- (INTEGER) the 4-digit year in which the games were held (e.g. 1992)
-    season -- (TEXT) the season of the games (either "Summer" or "Winter")
-    city -- (TEXT) the host city (e.g. "Barcelona")
-'''
+#     id -- (INTEGER) a unique identifier for the games in question
+#     year -- (INTEGER) the 4-digit year in which the games were held (e.g. 1992)
+#     season -- (TEXT) the season of the games (either "Summer" or "Winter")
+#     city -- (TEXT) the host city (e.g. "Barcelona")
+# '''
 @app.route('/games')
 def get_games():
+    connection = get_connection()
 
     # SQL Call
     try:
@@ -64,6 +67,7 @@ def get_games():
 
         olympics_games_list.append(olympics_game)
 
+    connection.close()    
     return json.dumps(olympics_games_list)
 
 
@@ -79,6 +83,8 @@ def get_games():
 '''
 @app.route('/nocs')
 def get_nocs():
+
+    connection = get_connection()
 
     # SQL Call
     try:
@@ -97,43 +103,78 @@ def get_nocs():
         noc_region['abbreviation'] = row[0]
         noc_region['name'] = row[1]
 
-
         noc_regions_list.append(noc_region)
 
+    connection.close()    
     return json.dumps(noc_regions_list)
 
-@app.route('/movies')
-def get_movies():
-    ''' Returns the list of movies that match GET parameters:
-        start_year, int: reject any movie released earlier than this year
-        end_year, int: reject any movie released later than this year
-        genre: reject any movie whose genre does not match this genre exactly
-        If a GET parameter is absent, then any movie is treated as though
-        it meets the corresponding constraint. (That is, accept a movie unless
-        it is explicitly rejected by a GET parameter.)
-    '''
-    movie_list = []
-    genre = flask.request.args.get('genre')
-    start_year = flask.request.args.get('start_year', default=0, type=int)
-    end_year = flask.request.args.get('end_year', default=10000, type=int)
-    for movie in movies:
-        if genre is not None and genre != movie['genre']:
-            continue
-        if movie['year'] < start_year:
-            continue
-        if movie['year'] > end_year:
-            continue
-        movie_list.append(movie)
-
-    return json.dumps(movie_list)
 
 
-@app.route('/help')
-def get_help():
-    return flask.render_template('help.html')
+'''
+    REQUEST: /medalists/games/<games_id>?[noc=noc_abbreviation]
+
+    RESPONSE: a JSON list of dictionaries, each representing one athlete
+    who earned a medal in the specified games. Each dictionary will have the
+    following fields.
+
+    athlete_id -- (INTEGER) a unique identifier for the athlete
+    athlete_name -- (TEXT) the athlete's full name
+    athlete_sex -- (TEXT) the athlete's sex as specified in the database ("F" or "M")
+    sport -- (TEXT) the name of the sport in which the medal was earned
+    event -- (TEXT) the name of the event in which the medal was earned
+    medal -- (TEXT) the type of medal ("gold", "silver", or "bronze")
+
+    If the GET parameter noc=noc_abbreviation is present, this endpoint will return
+    only those medalists who were on the specified NOC's team during the specified
+    games.
+'''
+@app.route('/medalists/games/<games_id>')
+def get_medalists(games_id):
+
+
+    connection = get_connection()
+
+    noc = flask.request.args.get('noc')
+
+    query = '''SELECT athlete.id, athlete.whole_name, athlete.sex, event.sport, event.athletic_event, athlete_NOC_event_game_metal.metal
+    FROM athlete, event, athlete_NOC_event_game_metal, game
+    WHERE athlete_NOC_event_game_metal.event_ID = event.id
+    AND athlete_NOC_event_game_metal.athlete_ID = athlete.id 
+    AND athlete_NOC_event_game_metal.game_ID = game.ID
+    AND game.ID = ''' + games_id
+
+    # if noc:
+    #     query += ' AND athlete_NOC_event_game_metal.NOC_abbreviation = ' + noc
+    
+    query += ' ORDER BY athlete.whole_name;'
+
+    # SQL Call
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+    except Exception as e:
+        print(e)
+        exit()
+    # Convert Results to JSON format
+    athletes_list = []
+    for row in cursor:
+        athlete = {}
+        athlete['athlete_id'] = int(row[0])
+        athlete['athlete_name'] = row[1]
+        athlete['athlete_sex'] = row[2]
+        athlete['sport'] = row[3]        
+        athlete['event'] = row[4]
+        athlete['medal'] = row[5]
+
+        if athlete['medal'] != "NA":
+            athletes_list.append(athlete)
+
+    connection.close()    
+    return json.dumps(athletes_list)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('A sample Flask application/API')
+    parser = argparse.ArgumentParser('A Flask application/API to handle the olympics database')
     parser.add_argument('host', help='the host on which this application is running')
     parser.add_argument('port', type=int, help='the port on which this application is listening')
     arguments = parser.parse_args()
